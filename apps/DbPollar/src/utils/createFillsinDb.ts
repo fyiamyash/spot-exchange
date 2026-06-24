6e7812759b51bc7bb7d31b843d4166aca701ff73
fyiamyash <yashmeshram786@gmail.com>
2026-06-24 23:58:50 +0530

    status:bugFix

--- apps/DbPollar/src/utils/createFillsinDb.ts
+++ apps/DbPollar/src/utils/createFillsinDb.ts
@@ -15,6 +15,40 @@ export function createFills(incomingFills: fillsType[]) {
           createdAt: element.createdAt,
         },
       });
+
+      const existingOrder = await prisma.orders.findFirst({
+        where: {
+          id: element.makerOrderId,
+        },
+      });
+      if (!existingOrder) {
+        console.log("maker orderid doesnt exists!");
+        return -1;
+      }
+
+      if (
+        existingOrder.quantity -
+          existingOrder.filledQuantity -
+          element.quantity ===
+        0
+      ) {
+        await prisma.orders.update({
+          where: {
+            id: existingOrder.id,
+          },
+          data: {
+            id: existingOrder.id,
+            userId: existingOrder.userId,
+            price: existingOrder.price,
+            quantity: existingOrder.quantity,
+            side: existingOrder.side,
+            filledQuantity: existingOrder.filledQuantity + element.quantity,
+            status: "FILLED",
+            createdAt: existingOrder.createdAt,
+            market: existingOrder.market,
+          },
+        });
+      }
     } catch (err) {
       console.log("error while storing fills in the db", err);
     }
